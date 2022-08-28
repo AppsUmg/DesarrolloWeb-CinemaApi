995f9fc2bac54379baaec3f534dcda9786fdf7f5
Kev548 <72001201+Kev548@users.noreply.github.com>
2022-08-28 11:10:35 -0600

    Save 1

---
+++ API_REST_CINE/API_REST_CINE/Controllers/CinemaApiController.cs
@@ -0,0 +1,45 @@
+﻿using API_REST_CINE.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_REST_CINE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CinemaApiController : ControllerBase
+    {
+
+        [HttpPost]
+        public ActionResult setPelicula(string Tittle, string Year, string Type, string Img, string Ubication, string Description)
+        {
+            return this.Content(ClsConexion.setPelicula(Tittle,Year,Type,Img,Ubication,Description), "application/json", System.Text.Encoding.UTF8);
+        }
+
+        [HttpPut]
+        public ActionResult setModPelicula(int Id,string Tittle, string Year, string Type, string Img, string Ubication, string Description)
+        {
+            return this.Content(ClsConexion.setModificarPelicula(Id,Tittle, Year, Type, Img, Ubication, Description), "application/json", System.Text.Encoding.UTF8);
+        }
+        [HttpDelete]
+        public ActionResult setDelPelicula(int Id)
+        {
+            return this.Content(ClsConexion.setEliminarPelicula(Id), "application/json", System.Text.Encoding.UTF8);
+        }
+
+        [HttpGet("{value}")]
+        public ActionResult getFiltrarPelicula(string value)
+        {
+            return this.Content(ClsConexion.getFiltrarPelicula(value), "application/json", System.Text.Encoding.UTF8);
+        }
+
+        [HttpGet]
+        public ActionResult getPeliculas()
+        {
+            return this.Content(ClsConexion.getPeliculas(), "application/json", System.Text.Encoding.UTF8);
+        }
+    }
+}
---
+++ API_REST_CINE/API_REST_CINE/Data/ClsConexion.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_REST_CINE.Data
+{
+    public class ClsConexion
+    {
+        public static string connectionstring = "Data Source= umg2022.database.windows.net ; " +
+                                                "Initial Catalog= DB_UMG ;" +
+                                                "User = umg ;" +
+                                                "Password= Dev1234* ;" +
+                                                "app=API_REST_CINE";
+
+        public static string setPelicula(string Tittle, string Year, string Type, string Img, string Ubication,string Description)
+        {
+            string Result = "";
+            using (SqlConnection conn = new SqlConnection(connectionstring))
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+                da.SelectCommand = new SqlCommand("[USP_DCP_SET_PELICULA]", conn);
+                da.SelectCommand.CommandTimeout = 0;
+                da.SelectCommand.Parameters.AddWithValue("@Tittle", Tittle);
+                da.SelectCommand.Parameters.AddWithValue("@Year", Year);
+                da.SelectCommand.Parameters.AddWithValue("@Type", Type);
+                da.SelectCommand.Parameters.AddWithValue("@Img", Img);
+                da.SelectCommand.Parameters.AddWithValue("@Ubication", Ubication);
+                da.SelectCommand.Parameters.AddWithValue("@Description", Description);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.Fill(dt);
+                Result = CreateJson(dt);
+                conn.Dispose();
+                conn.Close();
+            }
+            return Result;
+        }
+
+        public static string setModificarPelicula(int Id, string Tittle, string Year, string Type, string Img, string Ubication, string Description)
+        {
+            string Result = "";
+            using (SqlConnection conn = new SqlConnection(connectionstring))
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+                da.SelectCommand = new SqlCommand("[USP_DCP_SET_MOD_PELICULA]", conn);
+                da.SelectCommand.CommandTimeout = 0;
+                da.SelectCommand.Parameters.AddWithValue("@Id", Id);
+                da.SelectCommand.Parameters.AddWithValue("@Tittle", Tittle);
+                da.SelectCommand.Parameters.AddWithValue("@Year", Year);
+                da.SelectCommand.Parameters.AddWithValue("@Type", Type);
+                da.SelectCommand.Parameters.AddWithValue("@Img", Img);
+                da.SelectCommand.Parameters.AddWithValue("@Ubication", Ubication);
+                da.SelectCommand.Parameters.AddWithValue("@Description", Description);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.Fill(dt);
+                Result = CreateJson(dt);
+                conn.Dispose();
+                conn.Close();
+            }
+            return Result;
+        }
+
+
+        public static string setEliminarPelicula(int Id)
+        {
+            string Result = "";
+            using (SqlConnection conn = new SqlConnection(connectionstring))
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+                da.SelectCommand = new SqlCommand("[USP_DCP_SET_DEL_PELICULA]", conn);
+                da.SelectCommand.CommandTimeout = 0;
+                da.SelectCommand.Parameters.AddWithValue("@Id", Id);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.Fill(dt);
+                Result = CreateJson(dt);
+                conn.Dispose();
+                conn.Close();
+            }
+            return Result;
+        }
+
+        public static string getPeliculas()
+        {
+            string Result = "";
+            using (SqlConnection conn = new SqlConnection(connectionstring))
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+                da.SelectCommand = new SqlCommand("[USP_DCP_GET_PELICULAS]", conn);
+                da.SelectCommand.CommandTimeout = 0;
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.Fill(dt);
+                Result = CreateJson(dt);
+                conn.Dispose();
+                conn.Close();
+            }
+            return Result;
+        }
+
+        public static string getFiltrarPelicula(string Value)
+        {
+            string Result = "";
+            using (SqlConnection conn = new SqlConnection(connectionstring))
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+                da.SelectCommand = new SqlCommand("[USP_DCP_GET_PELICULA]", conn);
+                da.SelectCommand.CommandTimeout = 0;
+                da.SelectCommand.Parameters.AddWithValue("@Value", Value);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.Fill(dt);
+                Result = CreateJson(dt);
+                conn.Dispose();
+                conn.Close();
+            }
+            return Result;
+        }
+
+
+        public static string CreateJson(DataTable table)
+        {
+            var JSONString = new StringBuilder();
+            if (table.Rows.Count > 0)
+            {
+                JSONString.Append("[");
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    JSONString.Append("{");
+                    for (int j = 0; j < table.Columns.Count; j++)
+                    {
+                        if (j < table.Columns.Count - 1)
+                        {
+                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + "\"" + table.Rows[i][j].ToString() + "\",");
+                        }
+                        else if (j == table.Columns.Count - 1)
+                        {
+                            JSONString.Append("\"" + table.Columns[j].ColumnName.ToString() + "\":" + "\"" + table.Rows[i][j].ToString() + "\"");
+                        }
+                    }
+                    if (i == table.Rows.Count - 1)
+                    {
+                        JSONString.Append("}");
+                    }
+                    else
+                    {
+                        JSONString.Append("},");
+                    }
+                }
+                JSONString.Append("]");
+            }
+            return JSONString.ToString();
+        }
+    }
+}
